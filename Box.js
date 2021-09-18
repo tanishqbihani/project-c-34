5553117226cad9adb9794ce9726bc14c7536e4ee
tanishqbihani <87018616+tanishqbihani@users.noreply.github.com>
2021-09-18 16:23:46 +0530

    Update Box.js

--- Box.js
+++ Box.js
@@ -1,7 +1,7 @@
 class Box {
   constructor(x, y, width, height) {
     var options = {
-        'restitution':0.8,
+        'restitution':1,
         'friction':1.0,
         'density':20,
          //modify the value of isStatic so that box objects can move
